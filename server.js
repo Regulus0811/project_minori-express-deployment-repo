153be89948be7efd960fc2bf79c05b183c97f009
Regulus0811 <kbc6268@g.yju.ac.kr>
2024-06-19 00:54:46 +0900

    ♻️ refactor: Overhaul Express server implementation
    Made comprehensive changes to the Express server to improve functionality and performance.
    
    Related issue: #4

--- server.js
+++ server.js
@@ -1,79 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const WebSocket = require('ws');
-// const url = require('url');
-
-// const app = express();
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
-
-// const rooms = {};
-
-// wss.on('connection', (ws, req) => {
-//   const query = url.parse(req.url, true).query;
-//   const classId = query.classId;
-//   const userId = query.userId;
-
-//   if (!rooms[classId]) {
-//     rooms[classId] = [];
-//   }
-//   rooms[classId].push({ userId, ws });
-
-//   console.log(`User ${userId} joined class ${classId}`);
-//   console.log(`Current users in class ${classId}:`, rooms[classId].map(client => client.userId));
-
-//   ws.on('message', (message) => {
-//     const data = JSON.parse(message);
-//     handleWebSocketMessage(data, classId, userId);
-//   });
-
-//   ws.on('close', () => {
-//     rooms[classId] = rooms[classId].filter(client => client.userId !== userId);
-//     if (rooms[classId].length === 0) {
-//       delete rooms[classId];
-//     }
-//     console.log(`User ${userId} left class ${classId}`);
-//   });
-// });
-
-// const handleWebSocketMessage = (data, classId, userId) => {
-//   const { event, data: eventData } = data;
-//   console.log(`Received ${event} from user ${userId} in class ${classId}`);
-//   switch (event) {
-//     case 'offer':
-//       broadcastToAll(classId, { event, data: eventData });
-//       break;
-//     case 'answer':
-//     case 'candidate':
-//       broadcastToOthers(classId, userId, { event, data: eventData });
-//       break;
-//     default:
-//       break;
-//   }
-// };
-
-// const broadcastToAll = (classId, message) => {
-//   if (rooms[classId]) {
-//     rooms[classId].forEach(client => {
-//       client.ws.send(JSON.stringify(message));
-//     });
-//   }
-// };
-
-// const broadcastToOthers = (classId, userId, message) => {
-//   if (rooms[classId]) {
-//     rooms[classId].forEach(client => {
-//       if (client.userId !== userId) {
-//         client.ws.send(JSON.stringify(message));
-//       }
-//     });
-//   }
-// };
-
-// server.listen(8080, () => {
-//   console.log('Server is listening on port 8080');
-// });
-
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
@@ -91,12 +15,12 @@ wss.on('connection', (ws, req) => {
   const userId = query.userId;
 
   if (!rooms[classId]) {
-    rooms[classId] = {};
+    rooms[classId] = [];
   }
-  rooms[classId][userId] = ws;
+  rooms[classId].push({ userId, ws });
 
   console.log(`User ${userId} joined class ${classId}`);
-  console.log(`Current users in class ${classId}:`, Object.keys(rooms[classId]));
+  console.log(`Current users in class ${classId}:`, rooms[classId].map(client => client.userId));
 
   ws.on('message', (message) => {
     const data = JSON.parse(message);
@@ -104,8 +28,8 @@ wss.on('connection', (ws, req) => {
   });
 
   ws.on('close', () => {
-    delete rooms[classId][userId];
-    if (Object.keys(rooms[classId]).length === 0) {
+    rooms[classId] = rooms[classId].filter(client => client.userId !== userId);
+    if (rooms[classId].length === 0) {
       delete rooms[classId];
     }
     console.log(`User ${userId} left class ${classId}`);
@@ -113,38 +37,36 @@ wss.on('connection', (ws, req) => {
 });
 
 const handleWebSocketMessage = (data, classId, userId) => {
-  const { event, data: eventData, to } = data;
+  const { event, data: eventData } = data;
   console.log(`Received ${event} from user ${userId} in class ${classId}`);
-
   switch (event) {
-    case 'join':
-      broadcastToOthers(classId, userId, { event: 'newUser', data: userId });
-      break;
     case 'offer':
+      broadcastToAll(classId, { event, data: eventData });
+      break;
     case 'answer':
     case 'candidate':
-      if (to) {
-        sendToUser(classId, to, { event, data: eventData, from: userId });
-      }
+      broadcastToOthers(classId, userId, { event, data: eventData });
       break;
     default:
       break;
   }
 };
 
-const broadcastToOthers = (classId, userId, message) => {
+const broadcastToAll = (classId, message) => {
   if (rooms[classId]) {
-    Object.keys(rooms[classId]).forEach(clientId => {
-      if (clientId !== userId) {
-        rooms[classId][clientId].send(JSON.stringify(message));
-      }
+    rooms[classId].forEach(client => {
+      client.ws.send(JSON.stringify(message));
     });
   }
 };
 
-const sendToUser = (classId, userId, message) => {
-  if (rooms[classId] && rooms[classId][userId]) {
-    rooms[classId][userId].send(JSON.stringify(message));
+const broadcastToOthers = (classId, userId, message) => {
+  if (rooms[classId]) {
+    rooms[classId].forEach(client => {
+      if (client.userId !== userId) {
+        client.ws.send(JSON.stringify(message));
+      }
+    });
   }
 };
 
